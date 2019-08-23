4eafd60de39d6b7de326a9cf5846a95da114f066
jwillits@illinois.edu <Bimsawa.1>
2019-08-23 11:21:44 -0500

    made life better

--- life.py
+++ life.py
@@ -8,6 +8,7 @@ class World:
 
     def __init__(self, matrix_size):
         self.the_matrix = None
+        self.new_matrix = None
         self.num_rows = matrix_size[0]
         self.num_columns = matrix_size[1]
 
@@ -15,61 +16,58 @@ class World:
 
     def init_matrix(self):
         self.the_matrix = np.zeros([self.num_rows, self.num_columns], int)
+        self.new_matrix = np.zeros([self.num_rows, self.num_columns], int)
 
         for i in range(self.num_rows):
             for j in range(self.num_columns):
                 if i == 0 or i == self.num_rows-1 or j == 0 or j == self.num_columns-1:
                     self.the_matrix[i,j] = 0
 
-        self.the_matrix[5, 5] = 1
-        self.the_matrix[5, 6] = 1
-        self.the_matrix[5, 7] = 1
+    def onclick_init(self, i, j, original):
+        if original == 0:
+            self.the_matrix[i, j] = 1
+        if original == 1:
+            self.the_matrix[i, j] = 0
 
     def next(self):
-
         for i in range(1, self.num_rows-1):
             for j in range(1, self.num_columns-1):
-
-                changed = False
 
                 num_on_neighbors = 0
 
                 for m in range(-1, 2):
                     for n in range(-1, 2):
                         num_on_neighbors += self.the_matrix[i + m, j + n]
-                        print(self.the_matrix[i+m, j+n], end="")
-                    print()
 
                 if self.the_matrix[i, j] == 1:
                     num_on_neighbors -= 1
 
-                    if 2 > num_on_neighbors > 3:
-                        self.the_matrix[i, j] = 0
+                    if 2 <= num_on_neighbors <= 3:
+                        self.new_matrix[i, j] = 1
                         changed = True
+                    else:
+                        self.new_matrix[i, j] = 0
                 else:
                     if num_on_neighbors == 3:
-                        self.the_matrix[i, j] = 1
+                        self.new_matrix[i, j] = 1
                         changed = True
-
-                print(i, j, num_on_neighbors)
-
-        self.print_matrix()
-
-    def print_matrix(self):
-        for i in range(self.num_rows):
-            for j in range(self.num_columns):
-                print(self.the_matrix[i, j], end='')
-            print()
-        print()
+                    else:
+                        self.new_matrix[i, j] = 0
+        tempor_matrix = self.the_matrix
+        self.the_matrix = self.new_matrix
+        self.new_matrix = tempor_matrix
 
 class Window:
 
-    def __init__(self, the_world):
+    def __init__(self, the_world, square_size):
         self.root = tk.Tk()
+        self.root.title("Conway's Game of Life")
 
         self.display_frame = tk.Frame(self.root)
         self.display_frame.pack()
-        self.canvas = tk.Canvas(self.display_frame, width=690, height=690)
+        self.canvas = tk.Canvas(self.display_frame, width=800, height=800)
+        self.canvas.pack()
+        self.canvas.bind("<Button-1>", self.onclick)
         self.canvas.pack()
 
         self.button_frame = tk.Frame(self.root)
@@ -87,6 +85,7 @@ class Window:
         self.square_matrix = None
 
         self.the_world = the_world
+        self.square_size = square_size
         self.running = False
 
         self.init_window()
@@ -96,6 +95,7 @@ class Window:
 
     def next(self):
         self.the_world.next()
+        self.destroy_world()
         self.draw_world()
 
     def start(self):
@@ -111,10 +111,30 @@ class Window:
             self.root.update()
             time.sleep(0.1)
 
-
     def reset(self):
         self.the_world.init_matrix()
+        self.destroy_world()
         self.draw_world()
+
+    def onclick(self, event):
+        x, y = event.x, event.y
+        i = int(x / self.square_size)
+        j = int(y / self.square_size)
+
+        if self.the_world.the_matrix[i,j] == 0:
+            original = 0
+            color = "yellow"
+            self.canvas.create_rectangle(i * self.square_size, j * self.square_size, (i + 1) * self.square_size, (j + 1) * self.square_size, fill=color)
+            self.the_world.onclick_init(i, j, original)
+        else:
+            original = 1
+            color = "grey"
+            self.canvas.create_rectangle(i * self.square_size, j * self.square_size, (i + 1) * self.square_size, (j + 1) * self.square_size, fill=color)
+            self.the_world.onclick_init(i, j, original)
+
+    def destroy_world(self):
+        self.square_matrix = None
+        self.canvas.delete("all")
 
     def draw_world(self):
         self.square_matrix = []
@@ -125,11 +145,11 @@ class Window:
                 if i == 0 or i == self.the_world.num_rows - 1 or j == 0 or j == self.the_world.num_columns - 1:
                     color = 'black'
                 else:
-                    if self.the_world.the_matrix[i,j] == 0:
+                    if self.the_world.new_matrix[i,j] == 0:
                         color = "grey"
                     else:
                         color = "yellow"
-                square = self.canvas.create_rectangle(j * 15, i * 15, (j + 1) * 15, (i + 1) * 15, fill=color)
+                square = self.canvas.create_rectangle(i * self.square_size, j * self.square_size, (i + 1) * self.square_size, (j + 1) * self.square_size, fill=color)
                 square_list.append(square)
                 j += 1
             self.square_matrix.append(square_list)
@@ -140,8 +160,9 @@ class Window:
 
 
 def main():
-    matrix_size = (12, 12)
+    matrix_size = (80, 80)
+    square_size = 10
     the_world = World(matrix_size)
-    the_window = Window(the_world)
+    the_window = Window(the_world, square_size)
     the_window.root.mainloop()
 main()
