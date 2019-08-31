05e40b0290f043e5dd770729659eb845f89192aa
jwillits@illinois.edu <Bimsawa.1>
2019-08-31 16:55:17 -0500

    created dataset class

--- semantic_network.py
+++ semantic_network.py
@@ -5,67 +5,32 @@ import numpy as np
 
 
 class Network:
-    def __init__(self):
-        self.selected_activation_amount = 5.0
-        self.spread_speed = 0.8
-        self.learning_rate = 0.1
-        self.selection_weight = 1
-        self.selected_node_array = None
-        self.load_data()
-        self.initialize_network()
-
-    def initialize_network(self):
-        self.activations = np.random.uniform(-.5, .5, [self.num_nodes])
-        self.weight_matrix = np.random.uniform(-.01, .01, [self.num_nodes, self.num_nodes])
+    def __init__(self, num_nodes):
+        self.num_nodes = num_nodes
+        self.activations = np.zeros([self.num_nodes], float)
+        self.weight_matrix = np.random.uniform(-1, 1, [self.num_nodes, self.num_nodes])
         self.training_steps = 0
         self.time_steps = 0
+        self.learning_rate = 0.1
 
-    def load_data(self):
-        f = open('semantic_network_items.txt')
-        line_counter = 0
-        training_data = []
-        for line in f:
-            data = (line.strip().strip('\n').strip()).split()
-            if line_counter == 0:
-                self.label_list = data
-            else:
-                training_data.append(np.array(data, float))
-            line_counter += 1
-        f.close()
-
-        self.training_data = np.array(training_data, float)
-        self.num_items = len(self.training_data)
-        self.num_nodes = len(self.training_data[0])
-        self.selected_node_array = np.zeros([self.num_nodes], float)
-        self.label_index_dict = {}
-        for i in range(self.num_nodes):
-            self.label_index_dict[self.label_list[i]] = i
-
-    def train(self):
+    def train(self, x):
         self.training_steps += 1
-        for i in range(self.num_items):
-            item = self.training_data[i]
+        for i in range(len(x)):
+            item = x[i]
             for j in range(self.num_nodes):
                 for k in range(self.num_nodes):
                     self.weight_matrix[j, k] += self.learning_rate * item[j] * item[k]
-        for i in range(self.num_nodes):
-            print(self.training_steps, self.label_list[i], self.weight_matrix[i,:])
+                print(self.training_steps, self.weight_matrix[i, :])
 
-    def next(self):
+    def next(self, selected_node_array):
         self.time_steps += 1
-        self.activations = np.tanh(np.dot(self.activations + self.selected_node_array*self.selection_weight,
-                                          self.weight_matrix * self.spread_speed))
-
-    def on_click(self, new_selected_index):
-        if self.selected_node_array[new_selected_index] == 1:
-            self.selected_node_array[new_selected_index] = 0
-        else:
-            self.selected_node_array[new_selected_index] = 1
+        self.activations = np.tanh(np.dot(self.activations+selected_node_array, self.weight_matrix))
 
 
 class Display:
-    def __init__(self, window_size, network):
+    def __init__(self, window_size, network, dataset):
         self.network = network
+        self.dataset = dataset
         self.height = window_size[0]
         self.width = window_size[1]
 
@@ -87,6 +52,7 @@ class Display:
         self.node_size = 20
         self.text_offset = [40, 10]
         self.weight_size = 6
+        self.selected_node_array = np.zeros([self.network.num_nodes], float)
 
         self.create_weight_frame()
         self.create_network_frame()
@@ -132,16 +98,18 @@ class Display:
     def network_click(self, event):
         x, y = event.x, event.y
         ids = self.network_canvas.find_overlapping(x, y, x, y)
-        try:
+        if len(ids) > 0:
             the_tag = self.network_canvas.itemcget(ids[0], "tags").split()[0]
-            the_index = self.network.label_index_dict[the_tag]
-            self.network.on_click(the_index)
-            self.update_network_activation_frame()
-        except:
-            pass
+            if the_tag in self.dataset.label_index_dict:
+                the_index = self.dataset.label_index_dict[the_tag]
+                if self.selected_node_array[the_index] == 1:
+                    self.selected_node_array[the_index] = 0
+                else:
+                    self.selected_node_array[the_index] = 1
+                self.update_network_activation_frame()
 
     def train(self):
-        self.network.train()
+        self.network.train(self.dataset.training_data)
         self.update_weight_frame()
 
     def start(self):
@@ -158,9 +126,10 @@ class Display:
             time.sleep(0.1)
 
     def next(self):
-        self.network.next()
+        self.network.next(self.selected_node_array)
         self.update_network_activation_frame()
         self.update_activation_graph_frame()
+        print(self.network.activations)
 
     def update_weight_frame(self):
         self.weight_canvas.delete("all")
@@ -174,7 +143,7 @@ class Display:
         spacing = 3
 
         for i in range(self.network.num_nodes):
-            self.weight_canvas.create_text(30, 23+i*9, font='Arial 7', text=self.network.label_list[i])
+            self.weight_canvas.create_text(30, 23+i*9, font='Arial 7', text=self.dataset.label_list[i])
             for j in range(self.network.num_nodes):
 
                 scaled_weight = self.network.weight_matrix[i, j] / 10
@@ -186,7 +155,6 @@ class Display:
 
                 self.weight_canvas.create_rectangle(x+j*(size+spacing), y+i*(size+spacing), x+j*(spacing+size)+size, y+i*(spacing+size)+size,
                                                  outline="black", fill=color, width=1)
-
 
     def update_activation_graph_frame(self):
         self.graph_canvas.delete("all")
@@ -201,28 +169,29 @@ class Display:
             coords = self.node_coordinate_list[i]
             color = self.get_hex_color(self.network.activations[i])
 
-            if self.network.selected_node_array[i] == 0:
+            if self.selected_node_array[i] == 0:
                 self.network_canvas.create_oval(coords[0],
                                                 coords[1],
                                                 coords[0] + self.node_size,
                                                 coords[1] + self.node_size,
-                                                outline="black", fill=color, width=1, tags=self.network.label_list[i])
-                self.network_canvas.create_text(coords[0] + self.text_offset[0] + 2*len(self.network.label_list[i]),
+                                                outline="black", fill=color, width=1, tags=self.dataset.label_list[i])
+                self.network_canvas.create_text(coords[0] + self.text_offset[0] + 2*len(self.dataset.label_list[i]),
                                                 coords[1] + self.text_offset[1],
-                                                text=self.network.label_list[i], font="Arial 10", fill='black')
+                                                text=self.dataset.label_list[i], font="Arial 10", fill='black')
             else:
                 self.network_canvas.create_oval(coords[0],
                                                 coords[1],
                                                 coords[0] + self.node_size,
                                                 coords[1] + self.node_size,
-                                                outline="black", fill=color, width=3, tags=self.network.label_list[i])
+                                                outline="black", fill=color, width=3, tags=self.dataset.label_list[i])
 
-                self.network_canvas.create_text(coords[0] + self.text_offset[0] + 2*len(self.network.label_list[i]),
+                self.network_canvas.create_text(coords[0] + self.text_offset[0] + 2*len(self.dataset.label_list[i]),
                                                 coords[1] + self.text_offset[1],
-                                                text=self.network.label_list[i], font="Arial 10 bold", fill='green')
+                                                text=self.dataset.label_list[i], font="Arial 10 bold", fill='green')
 
     def reset(self):
         self.network.initialize_network()
+        self.network.selected_node_array = np.zeros(self.network.num_nodes, float)
         self.update_weight_frame()
         self.update_activation_graph_frame()
         self.update_network_activation_frame()
@@ -248,12 +217,42 @@ class Display:
             return "#ffffff"
 
 
+class Dataset:
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.read_file()
+        self.create_summary_stats()
+
+    def read_file(self):
+        training_data = []
+        f = open(self.file_path)
+        line_counter = 0
+        for line in f:
+            data = (line.strip().strip('\n').strip()).split()
+            if line_counter == 0:
+                self.label_list = data
+            else:
+                training_data.append(np.array(data, float))
+            line_counter += 1
+        f.close()
+        self.training_data = np.array(training_data, float)
+
+    def create_summary_stats(self):
+        self.num_items = len(self.training_data)
+        self.num_features = len(self.label_list)
+        self.label_index_dict = {}
+        for i in range(self.num_features):
+            self.label_index_dict[self.label_list[i]] = i
+
+
 def main():
 
     window_size = (600, 1200)
-    the_network = Network()
+    the_dataset = Dataset('semantic_network_items.txt')
+    the_network = Network(the_dataset.num_features)
     np.set_printoptions(suppress=True, precision=3)
-    the_display = Display(window_size, the_network)
+    the_display = Display(window_size, the_network, the_dataset)
     the_display.root.mainloop()
 
 
